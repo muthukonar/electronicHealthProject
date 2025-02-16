1586401eed09709c2fbf625fe78f908aeff3e3be
gerdesjasonl <gerdesjasonl@gmail.com>
2025-02-16 16:29:07 -0500

    This is the completed patient-routes.ts with a dr-routes.ts with TODOs. Also updated index.ts

---
+++ server/src/routes/api/dr-routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { Doctor } from '../../models/index.js';
+
+const drRouter = express.Router();
+
+// GET /doctors - Get all doctors
+
+// GET /doctors/:doctor_id - Get a doctor by doctor_id
+
+// PUT /doctors/:doctor_id - Update a doctor by doctor_id
+
+
+// DELETE /doctors/:doctor_id - Delete a doctor by doctor_id
+
+

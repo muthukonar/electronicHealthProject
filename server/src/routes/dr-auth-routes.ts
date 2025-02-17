62fc5355d5eb47219ae844d1d4faa1d28b689f27
pechikonar <muthukonaruot@gmail.com>
2025-02-17 07:43:52 -0500

    dr-routes and dr-auth-routes code is completed for review

--- server/src/routes/dr-auth-routes.ts
+++ server/src/routes/dr-auth-routes.ts
@@ -3,40 +3,61 @@ import { Dr } from '../models/Doctors.js';  // Import the Dr model
 import jwt from 'jsonwebtoken';  // Import the JSON Web Token library
 import bcrypt from 'bcrypt';  // Import the bcrypt library for password hashing
 
-// Login function to authenticate a user
+// Login function to authenticate a dr
+export const drLogin = async (req: Request, res: Response) => {
+  const { email, password } = req.body;  // Extract username and password from request body
 
-// Find the user in the database by username
+  // Find the dr in the database by username
+  const dr = await Dr.findOne({
+    where: { email },
+  });
 
-// If user is not found, send an authentication failed response
+  // If dr is not found, send an authentication failed response
+  if (!dr) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
 
-// Compare the provided password with the stored hashed password
- 
-// If password is invalid, send an authentication failed response
+  // Compare the provided password with the stored hashed password
+  const drPasswordIsValid = await bcrypt.compare(password, dr.password);
+  // If password is invalid, send an authentication failed response
+  if (!drPasswordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
 
+  // Get the secret key from environment variables
+  const secretKey = process.env.JWT_SECRET_KEY || '';
 
-// Get the secret key from environment variables
+  // Generate a JWT token for the authenticated user
+  const token = jwt.sign({ email }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });  // Send the token as a JSON response
+};
 
+export const drSignUp = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const newDr = await Dr.create({ email, password });
 
-// Generate a JWT token for the authenticated user
-
+    console.log(newDr);
     
-// Get the secret key from environment variables
+      // Get the secret key from environment variables
+    const secretKey = process.env.JWT_SECRET_KEY || '';
 
+    // Generate a JWT token for the authenticated user
+    const token = jwt.sign({ username: newDr.email }, secretKey, { expiresIn: '1h' });
+    res.json({ token });  // Send the token as a JSON response
+    // res.status(201).json(newUser);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+}
 
-// Generate a JWT token for the authenticated user
-   
-// Send the token as a JSON response
-// res.status(201).json(newUser);
-  
-
-// Create a new router instance for doctor
-
+// Create a new router instance for dr
+const drAuthRouter = Router();
 
 // POST /login - Login a dr
-
-// Define the login route
+drAuthRouter.post('/drLogin', drLogin);  // Define the login route
 
 // POST /users - Create a new dr
+drAuthRouter.post('/drSignup', drSignUp);
 
-
-// Export the router instance
+export default drAuthRouter;  // Export the router instance

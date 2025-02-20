b6eccb3bd9f95a2e7b5e2784e69d7541b2803e8b
pechikonar <muthukonaruot@gmail.com>
2025-02-19 20:32:45 -0500

    added image url in seed data for patients and updated patient profile with logout button

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -12,11 +12,11 @@ export const seedUsers = async () => {
 
   // Seeding Patient data
   await Patient.bulkCreate([
-    {patient_name: 'John Doe',email: 'john@gmail.com',password: 'password',height: 180,weight: 75,age: 35,dr_id: 1,notes: 'Patient has a history of high blood pressure'},
-    {patient_name: 'Jane Smith',email: 'jane@gmail.com',password: 'password',height: 165,weight: 60,age: 28,dr_id: 2, notes: 'Patient has a history of skin allergies'},
-    {patient_name: 'Jolly Guru',email: 'jolly@gmail.com',password: 'password',height: 140,weight: 50,age: 16,dr_id: 3, notes: 'Patient has a history of migraines'},
-    {patient_name: 'Baby Guru',email: 'baby@gmail.com',password: 'password',height: 80,weight: 30,age: 9,dr_id: 4 , notes: 'Patient has a history of asthma'},
-    {patient_name: 'Kid Guru',email: 'kid@gmail.com',password: 'password',height: 10,weight: 10,age: 3,dr_id: 4 , notes: 'Patient has a history of cough'},
+    {patient_name: 'John Doe',email: 'john@gmail.com',password: 'password',height: 180,weight: 75,age: 35,dr_id: 1,notes: 'Patient has a history of high blood pressure',image_url:'https://media.licdn.com/dms/image/v2/D5603AQH1eGAgY2SKMA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712356138604?e=1745452800&v=beta&t=GaJ_6oznoNmYzY9GnvDy-dIhymN5Ib6knpwD0xB5cio'},
+    {patient_name: 'Jane Smith',email: 'jane@gmail.com',password: 'password',height: 165,weight: 60,age: 28,dr_id: 2, notes: 'Patient has a history of skin allergies',image_url:'https://media.licdn.com/dms/image/v2/D5603AQH1eGAgY2SKMA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712356138604?e=1745452800&v=beta&t=GaJ_6oznoNmYzY9GnvDy-dIhymN5Ib6knpwD0xB5cio'},
+    {patient_name: 'Jolly Guru',email: 'jolly@gmail.com',password: 'password',height: 140,weight: 50,age: 16,dr_id: 3, notes: 'Patient has a history of migraines',image_url:'https://media.licdn.com/dms/image/v2/D5603AQH1eGAgY2SKMA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712356138604?e=1745452800&v=beta&t=GaJ_6oznoNmYzY9GnvDy-dIhymN5Ib6knpwD0xB5cio'},
+    {patient_name: 'Baby Guru',email: 'baby@gmail.com',password: 'password',height: 80,weight: 30,age: 9,dr_id: 4 , notes: 'Patient has a history of asthma',image_url:'https://media.licdn.com/dms/image/v2/D5603AQH1eGAgY2SKMA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712356138604?e=1745452800&v=beta&t=GaJ_6oznoNmYzY9GnvDy-dIhymN5Ib6knpwD0xB5cio'},
+    {patient_name: 'Kid Guru',email: 'kid@gmail.com',password: 'password',height: 10,weight: 10,age: 3,dr_id: 4 , notes: 'Patient has a history of cough', image_url: 'https://media.licdn.com/dms/image/v2/D5603AQH1eGAgY2SKMA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712356138604?e=1745452800&v=beta&t=GaJ_6oznoNmYzY9GnvDy-dIhymN5Ib6knpwD0xB5cio'},
   ], { individualHooks: true });
 
   console.log('Seed data has been successfully inserted!');

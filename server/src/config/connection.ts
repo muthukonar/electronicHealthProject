b7dfb998dad57852abd02c6a77ad67fbea4f9bf4
pechikonar <muthukonaruot@gmail.com>
2025-02-21 10:04:37 -0500

    DBSeed issue with DB connection fixed

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,9 +1,32 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import { Sequelize } from 'sequelize';
+
+// console.log('DB_URL:', process.env.DB_PASSWORD);
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME || '',
+//       process.env.DB_USER || '',
+//       process.env.DB_PASSWORD,
+//       {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         dialectOptions: {
+//           decimalNumbers: true,
+//         },
+//       }
+//     );
+
+// export default sequelize;
+
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-console.log('DB_URL:', process.env.DB_PASSWORD);
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
